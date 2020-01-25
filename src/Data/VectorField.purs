c9d4648659e4eabda2918585753ad3edc07dc279
Michael Bock <me@thought2.de>
2020-01-25 09:48:00 +0100

    Remove tabs for recent purs compatibility

--- src/Data/VectorField.purs
+++ src/Data/VectorField.purs
@@ -5,12 +5,12 @@ import Data.Group (class Group)
 import Data.Semigroup.Commutative (class Commutative)
 
 class (Field k, Group (f k), Commutative (f k)) <= VectorField f k where
-	-- | - ∀v in V: one * v == v 
-	-- | - ∀a b in K, v in V: a * (b .* v) = (a * b) .* v
-	-- | - ∀a b in K, v in V: 
-	-- | 		- a .* (u + v) = a .* u + a .* v 
-	-- |  	- (a + b) .* v = a .* v + b .* v
-	scalarMul :: k -> f k -> f k
+    -- | - ∀v in V: one * v == v 
+    -- | - ∀a b in K, v in V: a * (b .* v) = (a * b) .* v
+    -- | - ∀a b in K, v in V: 
+    -- |      - a .* (u + v) = a .* u + a .* v 
+    -- |      - (a + b) .* v = a .* v + b .* v
+    scalarMul :: k -> f k -> f k
 
 flipScalarMul :: ∀f k. (VectorField f k) => f k -> k -> f k
 flipScalarMul = flip scalarMul
